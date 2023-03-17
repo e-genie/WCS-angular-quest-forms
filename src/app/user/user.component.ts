1831e368bff7a99efc45d184f726d94e70794f27
eugeniefroustey <eugenie.froustey@2moro.fr>
2023-03-17 17:34:38 +0100

    added quest 15 reactive form and formgroup

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { User } from '../models/user.model';
 
 @Component({
@@ -8,22 +8,29 @@ import { User } from '../models/user.model';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-  username = new FormControl('');
-  email = new FormControl('');
-  password = new FormControl('');
-  address = new FormControl('');
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: [''],
+      email: [''],
+      password: [''],
+      address: [''],
+    });
+  }
 
   submitted = false;
-  user = new User('', '', '', '');
 
-  Submit(): void {
+  users: any[] = [];
+
+  onSubmit() {
+    // Ajoute les données du formulaire au tableau users
+    this.users.push(this.userForm.value);
+
+    // Réinitialise le formulaire
+    this.userForm.reset();
+
     this.submitted = true;
-
-    (this.user.username = this.username.value),
-      (this.user.email = this.email.value),
-      (this.user.password = this.password.value),
-      (this.user.address = this.address.value);
-
-    console.log(this.user);
+    console.log(this.userForm.value);
   }
 }
